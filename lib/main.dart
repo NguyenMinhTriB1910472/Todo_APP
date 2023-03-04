b6c79d92623add1ecfdb0831b1c45915acf68ec3
minhtri228 <63711185+minhtri228@users.noreply.github.com>
2023-03-04 19:57:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:todo_list/app/modules/home/view.dart';
+
+void main() async{
+  await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Todo List',
+      home: const HomePage(),
+    );
+  }
+}
+
